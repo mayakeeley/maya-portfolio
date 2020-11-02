e3158d8f19ca1cb7e9a8960862d332d06bde78f0
maya <maya.keeley@outlook.com>
2020-11-02 18:05:10 +0000

    firebase hosting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'maya-portfolio';
+export class AppComponent implements OnInit {
+  title = `Maya's portfolio`;
+
+  public constructor(private titleService: Title) {}
+
+  public ngOnInit(): void {
+    this.setTitle(this.title);
+  }
+
+  public setTitle(newTitle: string): void {
+    this.titleService.setTitle(newTitle);
+  }
 }
